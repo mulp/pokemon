8039b4f3a1c1811884dfba2fba394d5e24e2624a
mulp <fabio.cuomo@gmail.com>
2020-11-23 21:49:34 +0100

    Rename test class

---
+++ PokemonTests/PokemonListLoaderTests.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonListLoaderTests.swift
+//  PokemonTests
+//
+//  Created by Fabio Cuomo on 23/11/2020.
+//
+
+import XCTest
+@testable import Pokemon
+
+class PokemonListLoaderTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
