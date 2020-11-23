bc5f861e121cb22946ec184a6df799183b11f653
mulp <fabio.cuomo@gmail.com>
2020-11-23 21:47:56 +0100

    Add list item domain model

---
+++ Pokemon/Domain/ListItem.swift
@@ -0,0 +1,13 @@
+//
+//  ListItem.swift
+//  Pokemon
+//
+//  Created by Fabio Cuomo on 23/11/2020.
+//
+
+import Foundation
+
+public struct ListItem {
+    let image: String
+    let name: String
+}
