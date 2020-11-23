60af1053e2bb5bf6208a4cc211cb705c563b5bd5
mulp <fabio.cuomo@gmail.com>
2020-11-23 22:21:49 +0100

    Remove duplication in test

--- PokemonTests/PokemonListLoaderTests.swift
+++ PokemonTests/PokemonListLoaderTests.swift
@@ -37,17 +37,13 @@ class PokemonListLoaderTests: XCTestCase {
     }
 
     func test_init_doesNotRequestLoadData() {
-        let url = URL(string: "http://a-valid.url.com")!
-        let client = HTTPClientSpy()
-        _ = PokemonListLoader(url: url, client: client)
-        
+        let (_, client) = makeSUT()
+
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        let url = URL(string: "http://a-valid.url.com")!
-        let client = HTTPClientSpy()
-        let sut = PokemonListLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
 
         sut.load()
         
@@ -56,6 +52,12 @@ class PokemonListLoaderTests: XCTestCase {
     
 
     // MARK: Helpers
+    func makeSUT(url: URL = URL(string: "http://a-valid.url.com")!) -> (sut: PokemonListLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = PokemonListLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
     class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
         
