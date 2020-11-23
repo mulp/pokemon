d5de7815d18c4d7c56cafa261a07b375084fa2e6
mulp <fabio.cuomo@gmail.com>
2020-11-23 23:23:16 +0100

    Refactor helper method expect to receive Result instead of Error

--- PokemonTests/PokemonListLoaderTests.swift
+++ PokemonTests/PokemonListLoaderTests.swift
@@ -87,7 +87,7 @@ class PokemonListLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompleteWithError: .connectivity) {
+        expect(sut, toCompleteWithResult: .failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -99,7 +99,7 @@ class PokemonListLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
 
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithError: .invalidData) {
+            expect(sut, toCompleteWithResult: .failure(.invalidData)) {
                 client.complete(withStatusCode: code, at: index)
             }
         }
@@ -108,7 +108,7 @@ class PokemonListLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompleteWithError: .invalidData) {
+        expect(sut, toCompleteWithResult: .failure(.invalidData)) {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
@@ -117,13 +117,10 @@ class PokemonListLoaderTests: XCTestCase {
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
 
-        var capturedResults = [PokemonListLoader.Result]()
-        sut.load { capturedResults.append($0) }
-
-        let emptyListJSON = Data("{\"results\": []}".utf8)
-        client.complete(withStatusCode: 200, data: emptyListJSON)
-
-        XCTAssertEqual(capturedResults, [.success([])])
+        expect(sut, toCompleteWithResult: .success([])) {
+            let emptyListJSON = Data("{\"results\": []}".utf8)
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        }
     }
 
     // MARK: Helpers
@@ -134,7 +131,7 @@ class PokemonListLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: PokemonListLoader,
-                        toCompleteWithError error: PokemonListLoader.Error,
+                        toCompleteWithResult result: PokemonListLoader.Result,
                         when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         
         var capturedResults = [PokemonListLoader.Result]()
@@ -142,7 +139,7 @@ class PokemonListLoaderTests: XCTestCase {
         
         action()
         
-        XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
+        XCTAssertEqual(capturedResults, [result], file: file, line: line)
     }
     
     class HTTPClientSpy: HTTPClient {
