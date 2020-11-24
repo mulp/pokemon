e62dead75f1d205f68ae16fddc1f610d1a87130d
mulp <fabio.cuomo@gmail.com>
2020-11-24 14:52:02 +0100

    Replace hardcoded 200 value with constant value

--- Pokemon/API/PokemonListLoader.swift
+++ Pokemon/API/PokemonListLoader.swift
@@ -54,8 +54,10 @@ private class ListItemsMapper {
         }
     }
 
+    static let OK_200: Int = 200
+
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [ListItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw PokemonListLoader.Error.invalidData
         }
 
