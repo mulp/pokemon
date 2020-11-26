e817dfe5c771cbeaabba6185f37d6f88bfeb6f84
mulp <fabio.cuomo@gmail.com>
2020-11-26 23:30:08 +0100

    Add public accessor to properties model and add public initialiser

--- Pokemon/Domain/DetailItem.swift
+++ Pokemon/Domain/DetailItem.swift
@@ -7,20 +7,38 @@
 
 import Foundation
 
-public struct DetailItem {
-    let id: Int
-    let name: String
-    let image: String
-    let stats: [Stats]
-    let types: [Types]
+public struct DetailItem: Equatable {
+    public let id: Int
+    public let name: String
+    public let image: String
+    public let stats: [Stats]
+    public let types: [Types]
+    
+    public init(id: Int, name: String, image: String, stats: [Stats], types: [Types]) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.stats = stats
+        self.types = types
+    }
 }
 
-public struct Stats {
+public struct Stats: Equatable {
     public let baseStats: Int
     public let name: String
+
+    public init(baseStats: Int, name: String) {
+        self.baseStats = baseStats
+        self.name = name
+    }
 }
 
-public struct Types {
+public struct Types: Equatable {
     public let slot: Int
     public let name: String
+    
+    public init(slot: Int, name: String) {
+        self.slot = slot
+        self.name = name
+    }
 }
