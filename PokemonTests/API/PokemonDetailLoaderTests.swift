dce85d65ae9f870e3b5d32d20f4847fdf53ba295
mulp <fabio.cuomo@gmail.com>
2020-11-26 23:34:09 +0100

    Move HTTPClientSpy to shared file to avoid duplication

--- PokemonTests/API/PokemonDetailLoaderTests.swift
+++ PokemonTests/API/PokemonDetailLoaderTests.swift
@@ -162,31 +162,5 @@ class PokemonDetailLoaderTests: XCTestCase {
             "types": [type1JSON, type2JSON]
         ]
         return (item, json)
-    }
-    
-    class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success(data, response))
-        }
-    }
+    }    
 }
--- PokemonTests/API/PokemonListLoaderTests.swift
+++ PokemonTests/API/PokemonListLoaderTests.swift
@@ -140,31 +140,5 @@ class PokemonListLoaderTests: XCTestCase {
         action()
         
         XCTAssertEqual(capturedResults, [result], file: file, line: line)
-    }
-    
-    class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success(data, response))
-        }
-    }
+    }    
 }
